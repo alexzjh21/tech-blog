46cd8c5123b5e16c237d373f45d2f3d31a8d7787
alexzjh21 <alexzjh21@yahoo.co.jp>
2022-05-31 14:17:01 -0400

    finish setup

---
+++ utils/auth.js
@@ -0,0 +1,7 @@
+const withAuth = (req,res, next) => {
+    if(!req.session.user_id || req.session.loggedIn === false){
+        res.redirect('/login');
+    }else{next()};
+}
+
+module.exports = withAuth;
